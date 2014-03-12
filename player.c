66dd4c0f18616839bf3100c46689eddab975a581
Based Strawberry <666grapefruit@gmail.com>
2014-03-11 22:09:22 -0400

    fixed collision.

--- player.c
+++ player.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include "player.h"
 
-
-
 Player *initPlayer(char *name) {
 	Player *p = malloc(sizeof(Player));
 	p->ch = 64;
@@ -18,24 +16,28 @@ void drawPlayer(WINDOW *win,Player *p) {
 	mvwaddch(win,p->y,p->x,p->ch);
 }
 void updatePlayer(int i, Player *p, Map *m) {
+	int X = p->x-1;
+	int Y = p->y-1;
 	switch(i) {
 		case 97:
-			if((p->y*58)+p->x<=(16*58)-2 && m->grid[((p->y*58)+p->x)-2].walk) {
+			if(  (Y*58+(X-1))<=(16*58) && m->grid[Y*58+(X-1)].walk ) {
 				p->x -= 1;
 			}
 			break;
 		case 100:
-			if((p->y*58)+p->x<=(16*58) && m->grid[((p->y*58)+p->x)].walk) {
+			if(  (Y*58+(X+1))<=(16*58) && m->grid[Y*58+(X+1)].walk ) {
 				p->x += 1;
 			}
 			break;
 		case 119:
-			if((p->y*58)+p->x<=(16*58) && m->grid[((p->y*58)+p->x)].walk) {
+			if(  ((Y-1)*58+X)<=(16*58) && m->grid[(Y-1)*58+X].walk ) {
 				p->y -= 1;
 			}
 			break;
 		case 115:
-			p->y += 1;
+			if(  ((Y+1)*58+X)<=(16*58) && m->grid[(Y+1)*58+X].walk ) {
+				p->y += 1;
+			}
 			break;
 	}
 }
